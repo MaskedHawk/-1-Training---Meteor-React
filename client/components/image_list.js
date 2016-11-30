298bedbb3d136e1ff9fa749528f28c914d910dc1
Bollia Alexis <alexis@ollia.fr>
2016-11-30 13:02:26 +0100

    Create our image list component

---
+++ client/components/image_list.js
@@ -0,0 +1,18 @@
+/**
+ * Created by maskedhawk on 30/11/16.
+ */
+// Import React
+import React from 'react';
+
+// Create our component
+
+const ImageList = () => {
+    return (
+        <ul>
+            <li>Image Detail</li>
+        </ul>
+    );
+};
+
+// Export our component
+export default ImageList;
