155e8c932bca10c10e8d20255de1568227ec7a40
Bollia Alexis <alexis@ollia.fr>
2016-11-30 12:40:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#1 Try Meteor and React -
+Build an app witch show dynamically a list of image with title and description
---
+++ client/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by maskedhawk on 30/11/16.
+ */
+
+
+// Import REACT
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a component
+
+const App = () => {
+    return (
+        <div>
+            React App #2
+        </div>
+    );
+};
+
+// Render this component to the screen
+Meteor.startup(() => {
+    ReactDOM.render(<App />, document.querySelector('.container'));
+})
